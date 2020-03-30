6e3dd2d95c218e3b6e6ffd99fabda5df2b590693
cemahi <cem.ahi@freelancer.run>
2020-03-30 23:21:42 +0100

    Initial commit

---
+++ src/test/java/com/clearkode/exchange/ForeignExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clearkode.exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForeignExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
